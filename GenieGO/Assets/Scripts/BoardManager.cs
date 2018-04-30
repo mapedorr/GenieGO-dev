aac655008da5d9fc48d95f1adb0827a0425e3eff
Mateo Robayo <mapedorr@gmail.com>
2018-04-30 04:15:51 -0500

    Added assets and blank scripts
    Ready to start coding mvi.01

---
+++ GenieGO/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+	// ══════════════════════════════════════════════════════════════ PUBLICS ════
+	public static float spacing = 2f;
+	public static readonly Vector2[] directions = {
+		new Vector2 (spacing, 0f),
+		new Vector2 (-spacing, 0f),
+		new Vector2 (0f, spacing),
+		new Vector2 (0f, -spacing)
+	};
+	// ═══════════════════════════════════════════════════════════ PROPERTIES ════
+	// TODO: define properties
+	// ═════════════════════════════════════════════════════════════ PRIVATES ════
+	// TODO: define privates
+	// ══════════════════════════════════════════════════════════════ METHODS ════
+	// TODO: define methods
+}
